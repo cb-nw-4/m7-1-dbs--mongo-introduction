7432ff234a55562afbcb99093a288c8d6d600dae
“Alexandre <Alex_18h@hotmail.com>
2021-02-26 17:23:54 -0500

    exercise-2

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -1,14 +1,34 @@
 const assert = require('assert')
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+const { MONGO_URI } = process.env
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
 
 const createGreeting = async (req, res) => {
-  console.log(req.body)
   const client = await MongoClient(MONGO_URI, options)
-  await client.connect()
-  const db = client.db('exercise_1')
-  console.log('connected!')
-  await db.collection('users').insertOne(req.body)
+  try {
+    await client.connect()
+    const db = client.db('exercise_1')
+    console.log('connected!')
+    const result = await db.collection('greetings').insertOne(req.body)
+    assert.equal(1, result.insertedCount)
+
+    res.status(201).json({
+      status: 201,
+      data: req.body,
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      data: req.body,
+      message: err.message,
+    })
+  }
   client.close()
   console.log('disconnected!')
-  res.status(200).json('ok')
 }
 module.exports = { createGreeting }
