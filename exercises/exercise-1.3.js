f7c8d72453b5c9d737ac2e71377ca400214c09aa
“Alexandre <Alex_18h@hotmail.com>
2021-02-26 13:54:55 -0500

    exercise 1.3

--- exercises/exercise-1.3.js
+++ exercises/exercise-1.3.js
@@ -8,13 +8,26 @@ const options = {
   useUnifiedTopology: true,
 }
 
-const getCollection = async () => {
+const getUsers = async (req, res) => {
   const client = await MongoClient(MONGO_URI, options)
   await client.connect()
   const db = client.db('exercise_1')
   const users = await db.collection('users').find().toArray()
   console.log(users)
+
+  if (users.length === 0) {
+    res.status(404).json({
+      status: 404,
+      message: 'data not found',
+      data: users,
+    })
+  } else {
+    res.status(202).json({
+      status: 202,
+      message: `success`,
+    })
+  }
   client.close()
   console.log('disconnected!')
 }
-getCollection()
+module.exports = { getUsers }
