ac473bc71c7fbbc0537d8bdb0664e95fa3447ee4
“Alexandre <Alex_18h@hotmail.com>
2021-02-25 20:32:55 -0500

    exercise 1.2

--- exercises/exercise-1.1.js
+++ exercises/exercise-1.1.js
@@ -8,17 +8,17 @@ const options = {
     useUnifiedTopology: true,
 };
 
-const dbFunction = async (exercise_1) => {
+const dbFunction = async () => {
     // creates a new client
     const client = await MongoClient(MONGO_URI, options);
     // connect to the client
     await client.connect();
     // connect to the database (db name is provided as an argument to the function)
-    const db = client.db(exercise_1);
+    const db = client.db("exercise_1");
     console.log("connected!");
     await db.collection("users").insertOne({ name: "Buck Rogers" });
     // close the connection to the database server
     client.close();
     console.log("disconnected!");
 };
-return dbFunction()
+dbFunction()
