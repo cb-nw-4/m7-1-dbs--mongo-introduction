f6c6c6ea85c6907c761833f2d42beb526d1efb64
ferielnabet <fnabet@ipconnex.com>
2021-02-27 11:10:27 -0500

    update

--- exercises/exercise-1.1.js
+++ exercises/exercise-1.1.js
@@ -17,7 +17,7 @@ const dbFunction = async(dbName) =>{
 
     const db = client.db(dbName);
     console.log('connected!')
-    await db.collection("users").insertOne({ name: "Buck Rogers" });
+    await db.collection("users").insertOne({ name: "Feriel" });
 
     //close the connection to the database server
 
