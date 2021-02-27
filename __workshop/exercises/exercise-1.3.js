e2e3decb9839e69276cd086a7d3772736dce432e
Vanessa Chan <chw.vanessa@gmail.com>
2021-02-26 20:48:07 -0500

    Ex 1.3

--- __workshop/exercises/exercise-1.3.js
+++ __workshop/exercises/exercise-1.3.js
@@ -8,12 +8,28 @@ const options = {
   useUnifiedTopology: true,
 };
 
-const getCollection = async (dbName) => {
+const getUsers = async (req, res) => {
   let client = await MongoClient(MONGO_URI, options);
   await client.connect();
-  const db = client.db(dbName);
+  const db = client.db("exercise_1");
   const data = await db.collection("users").find().toArray();
   console.log(data);
   client.close();
+
+  if (data) {
+    res.status(200).json({
+      status: 200,
+      data: data,
+      message: "Request successful",
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      error: `No match found`,
+    });
+  }
 };
-getCollection("exercise_1");
+
+module.exports = {
+  getUsers,
+};
