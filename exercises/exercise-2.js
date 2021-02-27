701928dbdac3dc31ac132600e64e4318b215f19a
Mallory Belgica <mallorybelgica@gmail.com>
2021-02-27 14:22:04 -0500

    Completed exercise 2

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -41,7 +41,7 @@ const createGreeting = async (req, res) => {
 
 const getGreeting = async (req, res) => {
   const dbName = "exercise_1";
-  const _id = req.params._id;
+  const _id = req.params._id.toUpperCase();
 
   await client.connect();
 
@@ -56,4 +56,85 @@ const getGreeting = async (req, res) => {
   });
 };
 
-module.exports = { createGreeting, getGreeting };
+const getGreetings = async (req, res) => {
+  const dbName = "exercise_1";
+
+  await client.connect();
+
+  const db = client.db(dbName);
+  console.log("connected");
+
+  const greetings = await db.collection("greetings").find().toArray();
+
+  const { start = 1, limit = 25 } = req.query;
+
+  const startIndex = (start - 1) * limit;
+  const endIndex = start * limit;
+
+  const slicedGreetings = Object.values(greetings).slice(startIndex, endIndex);
+  const numLimits = Math.ceil(greetings.length / limit);
+
+  res.status(200).json({ status: 200, start, limit, data: slicedGreetings });
+};
+
+const deleteGreeting = async (req, res) => {
+  try {
+    const dbName = "exercise_1";
+    const _id = req.params._id.toUpperCase();
+
+    await client.connect();
+
+    const db = client.db(dbName);
+    console.log("connected");
+
+    const greetings = await db.collection("greetings").find().toArray();
+
+    const result = await db.collection("greetings").deleteOne({ _id });
+    assert.equal(1, result.deletedCount);
+
+    res
+      .status(201)
+      .json({ status: 204, message: "Greeting successfully deleted" });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, message: err.message });
+  }
+
+  client.close();
+  console.log("disconnected!");
+};
+
+const updateGreeting = async (req, res) => {
+  try {
+    const dbName = "exercise_1";
+    const _id = req.params._id;
+    const newValues = { $set: { hello: req.body.hello } };
+
+    await client.connect();
+
+    const db = client.db(dbName);
+    console.log("connected");
+
+    const results = await db
+      .collection("greetings")
+      .updateOne({ _id }, newValues);
+    assert.equal(1, results.matchedCount);
+    assert.equal(1, results.modifiedCount);
+
+    res.status(201).json({ status: 204, message: results });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, message: err.message });
+  }
+
+  client.close();
+  console.log("disconnected!");
+};
+
+module.exports = {
+  createGreeting,
+  getGreeting,
+  getGreetings,
+  deleteGreeting,
+  updateGreeting,
+};
--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { getUsers } = require("./exercises/exercise-1.3");
 const { addUser } = require("./exercises/exercise-1.4");
-const { createGreeting, getGreeting } = require("./exercises/exercise-2");
+const {
+  createGreeting,
+  getGreeting,
+  getGreetings,
+  deleteGreeting,
+  updateGreeting,
+} = require("./exercises/exercise-2");
 
 const PORT = process.env.PORT || 8000;
 
@@ -23,6 +29,9 @@ express()
   // exercise 2
   .post("/exercise-2/greeting", createGreeting)
   .get("/exercise-2/greeting/:_id", getGreeting)
+  .get("/exercise-2/greetings", getGreetings)
+  .delete("/exercise-2/greeting/:_id", deleteGreeting)
+  .put("/exercise-2/greeting/:_id", updateGreeting)
 
   // handle 404s
   .use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
