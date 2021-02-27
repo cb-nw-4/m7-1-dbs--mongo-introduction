054417389b9a085618bb72d76c2d064636c4cee3
Joni Sadler <joni.anne.sadler@gmail.com>
2021-02-26 19:15:38 -0500

    Exercise 2.3 complete

--- exercises/exercise-2.js
+++ exercises/exercise-2.js
@@ -31,4 +31,24 @@ const createGreeting = async (req, res) => {
   console.log("disconnected!");
 }
 
-module.exports = { createGreeting };
+
+const getGreeting = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);  
+  
+  await client.connect();
+
+  const db = client.db("greetings");
+  console.log("connected!");
+
+  const _id = req.params._id;
+  console.log(_id);
+
+  db.collection("greetings").findOne({ _id }, (err, result) => {
+    result
+      ? res.status(200).json({ status: 200, _id, data: result })
+      : res.status(404).json({ status: 404, _id, data: "Not Found" });
+    client.close();
+  })
+}
+
+module.exports = { createGreeting, getGreeting };
